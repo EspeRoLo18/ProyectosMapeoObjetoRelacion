85c4212297b0ecddefae379e52ddf1b6b4747eaa
Espe Romero <43826216+EspeRoLo18@users.noreply.github.com>
2021-03-13 03:31:37 -0600

    Update
    new project hibernatexml

---
+++ HibernateXML/src/main/java/mx/edu/uacm/hibernatexml/dominio/Usuario.java
@@ -0,0 +1,36 @@
+package mx.edu.uacm.hibernatexml.dominio;
+
+public class Usuario {
+	
+	private Long id;
+	private String nombreUsuario;
+	private String mensajeUsuario;
+
+	public Usuario() {
+
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getMensajeUsuario() {
+		return mensajeUsuario;
+	}
+
+	public void setMensajeUsuario(String mensajeUsuario) {
+		this.mensajeUsuario = mensajeUsuario;
+	}
+}
